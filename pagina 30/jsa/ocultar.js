d48edc928eaa21d4be98a866de6e2b030f9a080a
NatoVc <147545080+NatoVc@users.noreply.github.com>
2024-05-31 13:04:27 +0200

    Add files via upload

---
+++ pagina 30/jsa/ocultar.js
@@ -0,0 +1,121 @@
+// cada parrafo tiene su propia funcion
+function funcion1(){
+    let contenidooculto = document.getElementById('contenido-oculto');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion2(){
+    let contenidooculto = document.getElementById('a');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion3(){
+    let contenidooculto = document.getElementById('b');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion4(){
+    let contenidooculto = document.getElementById('c');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion5(){
+    let contenidooculto = document.getElementById('d');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion6(){
+    let contenidooculto = document.getElementById('e');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion7(){
+    let contenidooculto = document.getElementById('f');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion8(){
+    let contenidooculto = document.getElementById('g');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion9(){
+    let contenidooculto = document.getElementById('h');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion10(){
+    let contenidooculto = document.getElementById('i');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
+
+function funcion11(){
+    let contenidooculto = document.getElementById('j');
+    if (contenidooculto.style.display !== 'block') {
+        contenidooculto.style.display = 'block';
+        document.getElementById('mostrar-boton').innerText = 'Ocultar Contenido';
+    } else {
+        contenidooculto.style.display = 'none';
+        document.getElementById('mostrar-boton').innerText = 'Mostrar Contenido';
+    }
+}
